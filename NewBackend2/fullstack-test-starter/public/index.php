0f50efc0cf50bb8c4a9ddc314f8608f978561569
Lauris <Lauris2232@gmail.com>
2025-06-22 04:53:08 +0300

    cors debug

--- NewBackend2/fullstack-test-starter/public/index.php
+++ NewBackend2/fullstack-test-starter/public/index.php
@@ -1,11 +1,15 @@
 <?php
 
+// Устанавливаем CORS заголовки
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Accept, Origin');
+header('Access-Control-Max-Age: 86400');
+header('Content-Type: application/json; charset=utf-8');
 
 // Обработка preflight запросов
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
     exit(0);
 }
 
